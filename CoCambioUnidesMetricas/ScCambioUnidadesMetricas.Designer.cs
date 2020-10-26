ae0f906e6b001623b5bb51e9542e2d4e3272e8b7
NelsonJfsg <nelin@DESKTOP-I3QOV30>
2020-10-26 12:00:44 -0700

    Agregar archivos de proyecto.

---
+++ CoCambioUnidesMetricas/ScCambioUnidadesMetricas.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace CoCambioUnidesMetricas
+{
+    partial class ScCambioUnidadesMetricas
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LSubTitulo = new System.Windows.Forms.Label();
+            this.LTitulo = new System.Windows.Forms.Label();
+            this.Gb1 = new System.Windows.Forms.GroupBox();
+            this.RbKilometros1 = new System.Windows.Forms.RadioButton();
+            this.RbMetros1 = new System.Windows.Forms.RadioButton();
+            this.RbCentimetros1 = new System.Windows.Forms.RadioButton();
+            this.RbMilimetros1 = new System.Windows.Forms.RadioButton();
+            this.TbUnidad = new System.Windows.Forms.TextBox();
+            this.Gb2 = new System.Windows.Forms.GroupBox();
+            this.RbKilometros2 = new System.Windows.Forms.RadioButton();
+            this.RbMetros2 = new System.Windows.Forms.RadioButton();
+            this.RbCentimetros2 = new System.Windows.Forms.RadioButton();
+            this.RbMilimetros2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TbResultado = new System.Windows.Forms.TextBox();
+            this.Gb1.SuspendLayout();
+            this.Gb2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LSubTitulo
+            // 
+            this.LSubTitulo.AutoSize = true;
+            this.LSubTitulo.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LSubTitulo.Location = new System.Drawing.Point(157, 42);
+            this.LSubTitulo.Name = "LSubTitulo";
+            this.LSubTitulo.Size = new System.Drawing.Size(171, 25);
+            this.LSubTitulo.TabIndex = 1;
+            this.LSubTitulo.Text = "Unidad a convertir";
+            // 
+            // LTitulo
+            // 
+            this.LTitulo.AutoSize = true;
+            this.LTitulo.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTitulo.Location = new System.Drawing.Point(106, 12);
+            this.LTitulo.Name = "LTitulo";
+            this.LTitulo.Size = new System.Drawing.Size(301, 30);
+            this.LTitulo.TabIndex = 4;
+            this.LTitulo.Text = "Cambio de unidades metricas";
+            // 
+            // Gb1
+            // 
+            this.Gb1.Controls.Add(this.RbKilometros1);
+            this.Gb1.Controls.Add(this.RbMetros1);
+            this.Gb1.Controls.Add(this.RbCentimetros1);
+            this.Gb1.Controls.Add(this.RbMilimetros1);
+            this.Gb1.Location = new System.Drawing.Point(14, 70);
+            this.Gb1.Name = "Gb1";
+            this.Gb1.Size = new System.Drawing.Size(183, 121);
+            this.Gb1.TabIndex = 0;
+            this.Gb1.TabStop = false;
+            this.Gb1.Text = "Unidad metrica";
+            // 
+            // RbKilometros1
+            // 
+            this.RbKilometros1.AutoSize = true;
+            this.RbKilometros1.Location = new System.Drawing.Point(7, 89);
+            this.RbKilometros1.Name = "RbKilometros1";
+            this.RbKilometros1.Size = new System.Drawing.Size(73, 17);
+            this.RbKilometros1.TabIndex = 3;
+            this.RbKilometros1.TabStop = true;
+            this.RbKilometros1.Text = "Kilometros";
+            this.RbKilometros1.UseVisualStyleBackColor = true;
+            // 
+            // RbMetros1
+            // 
+            this.RbMetros1.AutoSize = true;
+            this.RbMetros1.Location = new System.Drawing.Point(7, 66);
+            this.RbMetros1.Name = "RbMetros1";
+            this.RbMetros1.Size = new System.Drawing.Size(57, 17);
+            this.RbMetros1.TabIndex = 2;
+            this.RbMetros1.TabStop = true;
+            this.RbMetros1.Text = "Metros";
+            this.RbMetros1.UseVisualStyleBackColor = true;
+            // 
+            // RbCentimetros1
+            // 
+            this.RbCentimetros1.AutoSize = true;
+            this.RbCentimetros1.Location = new System.Drawing.Point(7, 43);
+            this.RbCentimetros1.Name = "RbCentimetros1";
+            this.RbCentimetros1.Size = new System.Drawing.Size(80, 17);
+            this.RbCentimetros1.TabIndex = 1;
+            this.RbCentimetros1.TabStop = true;
+            this.RbCentimetros1.Text = "Centimetros";
+            this.RbCentimetros1.UseVisualStyleBackColor = true;
+            // 
+            // RbMilimetros1
+            // 
+            this.RbMilimetros1.AutoSize = true;
+            this.RbMilimetros1.Location = new System.Drawing.Point(7, 20);
+            this.RbMilimetros1.Name = "RbMilimetros1";
+            this.RbMilimetros1.Size = new System.Drawing.Size(71, 17);
+            this.RbMilimetros1.TabIndex = 0;
+            this.RbMilimetros1.TabStop = true;
+            this.RbMilimetros1.Text = "Milimetros";
+            this.RbMilimetros1.UseVisualStyleBackColor = true;
+            // 
+            // TbUnidad
+            // 
+            this.TbUnidad.Location = new System.Drawing.Point(82, 197);
+            this.TbUnidad.Multiline = true;
+            this.TbUnidad.Name = "TbUnidad";
+            this.TbUnidad.Size = new System.Drawing.Size(115, 24);
+            this.TbUnidad.TabIndex = 5;
+            this.TbUnidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Gb2
+            // 
+            this.Gb2.Controls.Add(this.RbKilometros2);
+            this.Gb2.Controls.Add(this.RbMetros2);
+            this.Gb2.Controls.Add(this.RbCentimetros2);
+            this.Gb2.Controls.Add(this.RbMilimetros2);
+            this.Gb2.Location = new System.Drawing.Point(292, 70);
+            this.Gb2.Name = "Gb2";
+            this.Gb2.Size = new System.Drawing.Size(183, 121);
+            this.Gb2.TabIndex = 4;
+            this.Gb2.TabStop = false;
+            this.Gb2.Text = "Unidad metrica";
+            // 
+            // RbKilometros2
+            // 
+            this.RbKilometros2.AutoSize = true;
+            this.RbKilometros2.Location = new System.Drawing.Point(7, 89);
+            this.RbKilometros2.Name = "RbKilometros2";
+            this.RbKilometros2.Size = new System.Drawing.Size(73, 17);
+            this.RbKilometros2.TabIndex = 3;
+            this.RbKilometros2.TabStop = true;
+            this.RbKilometros2.Text = "Kilometros";
+            this.RbKilometros2.UseVisualStyleBackColor = true;
+            // 
+            // RbMetros2
+            // 
+            this.RbMetros2.AutoSize = true;
+            this.RbMetros2.Location = new System.Drawing.Point(7, 66);
+            this.RbMetros2.Name = "RbMetros2";
+            this.RbMetros2.Size = new System.Drawing.Size(57, 17);
+            this.RbMetros2.TabIndex = 2;
+            this.RbMetros2.TabStop = true;
+            this.RbMetros2.Text = "Metros";
+            this.RbMetros2.UseVisualStyleBackColor = true;
+            // 
+            // RbCentimetros2
+            // 
+            this.RbCentimetros2.AutoSize = true;
+            this.RbCentimetros2.Location = new System.Drawing.Point(7, 43);
+            this.RbCentimetros2.Name = "RbCentimetros2";
+            this.RbCentimetros2.Size = new System.Drawing.Size(80, 17);
+            this.RbCentimetros2.TabIndex = 1;
+            this.RbCentimetros2.TabStop = true;
+            this.RbCentimetros2.Text = "Centimetros";
+            this.RbCentimetros2.UseVisualStyleBackColor = true;
+            // 
+            // RbMilimetros2
+            // 
+            this.RbMilimetros2.AutoSize = true;
+            this.RbMilimetros2.Location = new System.Drawing.Point(7, 20);
+            this.RbMilimetros2.Name = "RbMilimetros2";
+            this.RbMilimetros2.Size = new System.Drawing.Size(71, 17);
+            this.RbMilimetros2.TabIndex = 0;
+            this.RbMilimetros2.TabStop = true;
+            this.RbMilimetros2.Text = "Milimetros";
+            this.RbMilimetros2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(231, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "-->";
+            // 
+            // ButCalcular
+            // 
+            this.ButCalcular.Location = new System.Drawing.Point(203, 133);
+            this.ButCalcular.Name = "ButCalcular";
+            this.ButCalcular.Size = new System.Drawing.Size(83, 23);
+            this.ButCalcular.TabIndex = 7;
+            this.ButCalcular.Text = "Convertir";
+            this.ButCalcular.UseVisualStyleBackColor = true;
+            this.ButCalcular.Click += new System.EventHandler(this.ButCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Unidad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(288, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 21);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Resultado:";
+            // 
+            // TbResultado
+            // 
+            this.TbResultado.Location = new System.Drawing.Point(378, 197);
+            this.TbResultado.Multiline = true;
+            this.TbResultado.Name = "TbResultado";
+            this.TbResultado.Size = new System.Drawing.Size(97, 24);
+            this.TbResultado.TabIndex = 12;
+            this.TbResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ScCambioUnidadesMetricas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TbResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButCalcular);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Gb2);
+            this.Controls.Add(this.TbUnidad);
+            this.Controls.Add(this.LTitulo);
+            this.Controls.Add(this.LSubTitulo);
+            this.Controls.Add(this.Gb1);
+            this.Name = "ScCambioUnidadesMetricas";
+            this.Size = new System.Drawing.Size(493, 244);
+            this.Gb1.ResumeLayout(false);
+            this.Gb1.PerformLayout();
+            this.Gb2.ResumeLayout(false);
+            this.Gb2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label LSubTitulo;
+        private System.Windows.Forms.Label LTitulo;
+        private System.Windows.Forms.GroupBox Gb1;
+        private System.Windows.Forms.RadioButton RbKilometros1;
+        private System.Windows.Forms.RadioButton RbMetros1;
+        private System.Windows.Forms.RadioButton RbCentimetros1;
+        private System.Windows.Forms.RadioButton RbMilimetros1;
+        private System.Windows.Forms.TextBox TbUnidad;
+        private System.Windows.Forms.GroupBox Gb2;
+        private System.Windows.Forms.RadioButton RbKilometros2;
+        private System.Windows.Forms.RadioButton RbMetros2;
+        private System.Windows.Forms.RadioButton RbCentimetros2;
+        private System.Windows.Forms.RadioButton RbMilimetros2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TbResultado;
+    }
+}
